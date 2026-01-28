31a7242d1fda61085e5db90ab0cc390e50c72dac
CousemaAnjary <anjaryabdillah@gmail.com>
2026-01-28 17:10:22 +0300

    feat(frontend): ajouter le composant Button et la config Tailwind (aliases)

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   /**
    * ! STATE (état, données) de l'application
@@ -13,6 +15,7 @@ export default function Home() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Click me</Button>
     </>
   )
 }
