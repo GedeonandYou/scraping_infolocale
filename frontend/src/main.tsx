40635ac658dadce5422fb1b107d4fb224ae0e20b
CousemaAnjary <anjaryabdillah@gmail.com>
2026-02-03 15:35:26 +0300

    Update frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import 'leaflet/dist/leaflet.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 
@@ -9,6 +10,5 @@ createRoot(document.getElementById('root')!).render(
      <BrowserRouter>
       <App />
      </BrowserRouter>
-   
   </StrictMode>,
 )
